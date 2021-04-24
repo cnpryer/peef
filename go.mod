ff6c0ee1ec2a8a1e73afa6140de381c45b33fa4c
Rohan Nawathe <18901839+ObiWanRohan@users.noreply.github.com>
2021-04-25 02:40:43 +0530

    Moved to logrus package for logging and added logging to file

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/firediscordchat/peef
 
 go 1.16
 
-require github.com/bwmarrin/discordgo v0.23.3-0.20210327033043-f637c37ba2f0
+require (
+	github.com/bwmarrin/discordgo v0.23.3-0.20210327033043-f637c37ba2f0
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/sirupsen/logrus v1.8.1
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
+	golang.org/x/tools/gopls v0.6.10 // indirect
+)
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
-	"log"
 	"os"
 	"os/signal"
+
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/firediscordchat/peef/peef"
@@ -14,14 +16,32 @@ var (
 	GuildID        = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
 	BotToken       = flag.String("token", "", "Bot access token")
 	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
+	Debug          = flag.Bool("debug", false, "Run in debug mode")
+	LogFile        = flag.String("logfile", "peef-bot.log", "Log file name")
 )
 
 var s *discordgo.Session
+var log *logrus.Logger
 
 func init() {
+	log = logrus.New()
+	godotenv.Load()
 	flag.Parse()
 
-	var err error
+	// Setup loggers
+	log_file, err := os.OpenFile(*LogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Out = log_file
+
+	if *Debug {
+		log.Level = logrus.DebugLevel
+	} else {
+		log.Level = logrus.InfoLevel
+	}
+
 	s, err = discordgo.New("Bot " + *BotToken)
 	if err != nil {
 		log.Fatalf("Invalid bot parameters: %v", err)
@@ -35,16 +55,21 @@ func init() {
 }
 
 func main() {
-	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
-		log.Println("Bot is up!")
-	})
+	s.AddHandler(
+		func(s *discordgo.Session, r *discordgo.Ready) {
+			log.Info("Bot is up!")
+		},
+	)
+
 	err := s.Open()
 	if err != nil {
 		log.Fatalf("Cannot open the session: %v", err)
 	}
 
 	for _, v := range peef.Commands {
+
 		_, err := s.ApplicationCommandCreate(s.State.User.ID, *GuildID, v)
+
 		if err != nil {
 			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
 		}
@@ -55,5 +80,5 @@ func main() {
 	stop := make(chan os.Signal)
 	signal.Notify(stop, os.Interrupt)
 	<-stop
-	log.Println("Gracefully shutting down")
+	log.Info("Gracefully shutting down")
 }
